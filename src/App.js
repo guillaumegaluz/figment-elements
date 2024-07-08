a688fe665782c4cf0fd09cdfb0da3b1523bcc0ae
guillaumegaluz <guillaume.galuz@gmail.com>
2024-07-08 18:00:24 +0200

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+import { CopyBlock, dracula } from "react-code-blocks";
 
-function App() {
+function CodeBlock({ code }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ marginBottom: "20px" }}>
+      <CopyBlock
+        text={code}
+        language={"ts"}
+        theme={dracula}
+        showLineNumbers={3}
+      />
     </div>
   );
 }
 
+function App() {
+  return (
+    <Container>
+      <Left>
+        <LeftWrapper>
+          <CodeBlock code={importCode} />
+          <CodeBlock code={renderCode} />
+        </LeftWrapper>
+      </Left>
+      <Right>
+        {/* replace me with the StakeWidget import */}
+        <img alt="example" src="/example.png" width={350} />
+      </Right>
+    </Container>
+  );
+}
+
+const importCode = `import { StakeWidget } from "@figmentio/ui`;
+const renderCode = `<StakeWidget}
+  orgUuid="foobar" 
+  wallet={{
+    protocol: "ethereum",
+    address: "0x123"
+  }}
+  signer={() => foobar()}
+/>`;
+
 export default App;
+
+const Container = styled.div`
+  display: flex;
+  height: 100vh;
+`;
+
+const Left = styled.div`
+  width: 50%;
+  padding-top: 100px;
+  background-color: #eee;
+`;
+
+const LeftWrapper = styled.div`
+  width: 80%;
+  margin: 0 auto;
+`;
+
+const Right = styled.div`
+  width: 50%;
+  background-color: #ddd;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
