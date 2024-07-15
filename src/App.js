0103c335783187daa9fe658dffd77c249606b0d5
JesseKuntz <jessekuntz96@gmail.com>
2024-07-15 19:07:18 -0400

    feat: add figment elements script

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
+import React from "react";
 import styled from "styled-components";
 import { CopyBlock, dracula } from "react-code-blocks";
-import example from "./example.png";
 
 function CodeBlock({ code }) {
   return (
@@ -16,42 +16,99 @@ function CodeBlock({ code }) {
 }
 
 function App() {
+  React.useEffect(() => {
+    window.process = {
+      env: {
+        PLACEHOLDER: "placeholder",
+      },
+    };
+
+    window.FIGMENT_ELEMENTS_CONFIG = {
+      elements: [
+        {
+          containerSelector: "#figment-elements-staking-container",
+          element: "STAKING",
+        },
+        {
+          containerSelector: "#figment-elements-dapp-container",
+          element: "DAPP",
+        },
+      ],
+    };
+
+    const script = document.createElement("script");
+    script.src = "http://localhost:3098/elements";
+    script.type = "text/javascript";
+
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
   return (
     <Container>
       <Left>
         <LeftWrapper>
-          <CodeBlock code={importCode} />
+          <CodeBlock code={configCode} />
           <CodeBlock code={renderCode} />
         </LeftWrapper>
       </Left>
       <Right>
-        {/* replace me with the StakeWidget import */}
-        <img alt="example" src={example} width={350} />
+        <StakingContainer id="figment-elements-staking-container" />
+        <DappContainer id="figment-elements-dapp-container" />
       </Right>
     </Container>
   );
 }
 
-const importCode = `import { StakeWidget } from "@figmentio/ui`;
-const renderCode = `<StakeWidget}
-  orgUuid="foobar" 
-  wallet={{
-    protocol: "ethereum",
-    address: "0x123"
-  }}
-  signer={() => foobar()}
+const configCode = `React.useEffect(() => {
+  window.FIGMENT_ELEMENTS_CONFIG = {
+    elements: [
+      {
+        containerSelector: "#figment-elements-staking-container",
+        element: "STAKING",
+      },
+      {
+        containerSelector: "#figment-elements-dapp-container",
+        element: "DAPP",
+      },
+    ],
+  };
+
+  const script = document.createElement("script");
+  script.src = "https://dapp.figment.io/elements";
+  script.type = "text/javascript";
+
+  document.body.appendChild(script);
+
+  return () => {
+    document.body.removeChild(script);
+  };
+}, []);`;
+const renderCode = `<div
+  id="figment-elements-staking-container"
+  /* Add any custom styling necessary here */
+/>
+
+...
+
+<div
+  id="figment-elements-dapp-container"
+  /* Add any custom styling necessary here */
 />`;
 
 export default App;
 
 const Container = styled.div`
   display: flex;
-  height: 100vh;
+  min-height: 100vh;
 `;
 
 const Left = styled.div`
   width: 50%;
-  padding-top: 100px;
+  padding-top: 40px;
   background-color: #eee;
 `;
 
@@ -64,6 +121,18 @@ const Right = styled.div`
   width: 50%;
   background-color: #ddd;
   display: flex;
-  justify-content: center;
+  flex-direction: column;
   align-items: center;
+  padding: 40px;
+`;
+
+const StakingContainer = styled.div`
+  width: 380px;
+  height: 409px;
+`;
+
+const DappContainer = styled.div`
+  width: 800px;
+  height: 800px;
+  margin-top: 40px;
 `;
