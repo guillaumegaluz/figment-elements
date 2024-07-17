bd9960f49e114806ef4cb23ccb768ed3e839d5f4
JesseKuntz <jessekuntz96@gmail.com>
2024-07-17 14:57:06 -0400

    feat: add figment-elements package and separate script loading

--- src/App.js
+++ src/App.js
@@ -1,58 +1,138 @@
+import React from "react";
 import styled from "styled-components";
 import { CopyBlock, dracula } from "react-code-blocks";
-import example from "./example.png";
+import { Staking, Dapp } from "figment-elements";
 
 function CodeBlock({ code }) {
   return (
-    <div style={{ marginBottom: "20px" }}>
-      <CopyBlock
-        text={code}
-        language={"ts"}
-        theme={dracula}
-        showLineNumbers={3}
-      />
-    </div>
+    <CopyBlock
+      customStyle={{ marginTop: "20px", padding: "20px 0" }}
+      text={code}
+      language={"ts"}
+      theme={dracula}
+      showLineNumbers
+    />
   );
 }
 
 function App() {
+  React.useEffect(() => {
+    window.process = {
+      env: {
+        PLACEHOLDER: "placeholder",
+      },
+    };
+
+    window.FIGMENT_ELEMENTS_CONFIG = {
+      elements: [
+        {
+          containerSelector: "#figment-elements-staking-container",
+          element: "STAKING",
+        },
+        {
+          containerSelector: "#figment-elements-dapp-container",
+          element: "DAPP",
+        },
+      ],
+    };
+
+    const script = document.createElement("script");
+    script.src = "https://dapp.figment.io/elements";
+    script.type = "text/javascript";
+
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
   return (
-    <Container>
-      <Left>
-        <LeftWrapper>
-          <CodeBlock code={importCode} />
-          <CodeBlock code={renderCode} />
-        </LeftWrapper>
-      </Left>
-      <Right>
-        {/* replace me with the StakeWidget import */}
-        <img alt="example" src={example} width={350} />
-      </Right>
-    </Container>
+    <>
+      <Header>Figment Elements Demo</Header>
+      <Container>
+        <Left>
+          <LeftWrapper>
+            <SectionHeader>NPM Initialization</SectionHeader>
+            <CodeBlock code={npmImportCode} />
+            <CodeBlock code={npmRenderCode} />
+            <SectionHeader>Script Initialization</SectionHeader>
+            <CodeBlock code={scriptConfigCode} />
+            <CodeBlock code={scriptRenderCode} />
+          </LeftWrapper>
+        </Left>
+        <Right>
+          <SectionHeader>Output</SectionHeader>
+          <StakingContainer>
+            <Staking />
+          </StakingContainer>
+          <DappContainer>
+            <Dapp />
+          </DappContainer>
+        </Right>
+      </Container>
+    </>
   );
 }
 
-const importCode = `import { StakeWidget } from "@figmentio/ui`;
-const renderCode = `<StakeWidget}
-  orgUuid="foobar" 
-  wallet={{
-    protocol: "ethereum",
-    address: "0x123"
-  }}
-  signer={() => foobar()}
+const npmImportCode = `import { Staking, Dapp } from "figment-elements";`;
+const npmRenderCode = `<div /* Add any custom styling necessary here */>
+  <Staking />
+</div>
+
+...
+
+<div /* Add any custom styling necessary here */>
+  <Dapp />
+</div>`;
+const scriptConfigCode = `React.useEffect(() => {
+  window.FIGMENT_ELEMENTS_CONFIG = {
+    elements: [
+      {
+        containerSelector: "#figment-elements-staking-container",
+        element: "STAKING",
+      },
+      {
+        containerSelector: "#figment-elements-dapp-container",
+        element: "DAPP",
+      },
+    ],
+  };
+
+  const script = document.createElement("script");
+  script.src = "https://dapp.figment.io/elements";
+  script.type = "text/javascript";
+
+  document.body.appendChild(script);
+
+  return () => {
+    document.body.removeChild(script);
+  };
+}, []);`;
+const scriptRenderCode = `<div
+  id="figment-elements-staking-container"
+  /* Add any custom styling necessary here */
+/>
+
+...
+
+<div
+  id="figment-elements-dapp-container"
+  /* Add any custom styling necessary here */
 />`;
 
 export default App;
 
 const Container = styled.div`
   display: flex;
-  height: 100vh;
+  min-height: 100vh;
 `;
 
 const Left = styled.div`
   width: 50%;
-  padding-top: 100px;
+  padding-top: 40px;
   background-color: #eee;
+  padding: 40px 0;
 `;
 
 const LeftWrapper = styled.div`
@@ -64,6 +144,27 @@ const Right = styled.div`
   width: 50%;
   background-color: #ddd;
   display: flex;
-  justify-content: center;
+  flex-direction: column;
   align-items: center;
+  padding: 40px 0;
+`;
+
+const StakingContainer = styled.div`
+  width: 380px;
+  height: 409px;
+`;
+
+const DappContainer = styled.div`
+  width: 800px;
+  height: 800px;
+  margin-top: 40px;
+`;
+
+const Header = styled.h1`
+  font-size: 32px;
+  text-align: center;
+`;
+
+const SectionHeader = styled.h2`
+  font-size: 24px;
 `;
